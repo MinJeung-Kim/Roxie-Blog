d7391ba680a15797db117ceaa440a112332eb4fd
MinJeung-Kim <focso5@gmail.com>
2023-07-12 12:41:10 +0900

    Add: 다음 포스트 카드

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -11,7 +11,11 @@ export type Post = {
 };
 
 // Intersection Type
-export type PostData = Post & { content: string };
+export type PostData = Post & {
+  content: string;
+  next: Post | null;
+  prev: Post | null;
+};
 
 // ✨비즈니스 로직을 따로 함수로 사용하면 좋은 점.!!
 // getAllPosts() : 비동기 함수
@@ -51,11 +55,17 @@ export async function getAllPosts(): Promise<Post[]> {
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
-  const metadata = await getAllPosts() //
-    .then((posts) => posts.find((post) => post.path === fileName));
-  if (!metadata)
-    throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
 
+  if (!post) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+
+  // 현재 post의 위치
+  const index = posts.indexOf(post);
+  const next = index > 0 ? posts[index - 1] : null;
+  // 이전 post가 있다면 ? 이전 post 노출, 없다면 null
+  const prev = index < posts.length -1 ? posts[index + 1] : null; 
   const content = await readFile(filePath, "utf-8");
-  return { ...metadata, content };
+
+  return { ...post, content, next, prev };
 }
