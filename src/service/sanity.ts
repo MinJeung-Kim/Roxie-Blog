bd2423a5902e32eb4515ca88572dd3d8e1a7ff7b
MinJeung-Kim <focso5@gmail.com>
2024-02-02 03:02:39 +0900

    Fix

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -6,6 +6,6 @@ export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
   useCdn: false,
-  apiVersion: "2024-01-26",
+  apiVersion: "2024-02-02",
   token: process.env.SANITY_SECRET_TOKEN,
 });
