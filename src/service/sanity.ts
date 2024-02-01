0d150a5b9cd8be2c5614c07b5571e023b95834fd
MinJeung-Kim <focso5@gmail.com>
2024-02-02 03:22:08 +0900

    Fix: env

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 export const client = createClient({
   // Error : configuration must contain projectId
 
-  projectId: process.env.SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
   useCdn: false,
   apiVersion: "2024-02-02",
