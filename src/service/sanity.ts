f8a132d89bc3e42308fa9cb251eeaed3e1215596
MinJeung-Kim <focso5@gmail.com>
2024-02-02 02:57:10 +0900

    Fix : .env 파일에 변수를 저장할 때 공백

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
+  // Error : configuration must contain projectId
+
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
   useCdn: false,
