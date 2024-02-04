3aff11d748b1670de665b37c8d6d3fdf83307c4e
MinJeung-Kim <focso5@gmail.com>
2024-02-05 01:32:04 +0900

    최종

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -1,4 +1,6 @@
 import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
@@ -7,3 +9,13 @@ export const client = createClient({
   apiVersion: "2024-02-02",
   token: process.env.SANITY_SECRET_TOKEN,
 });
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source).width(800).url();
+}
+
+// https://www.sanity.io/docs/http-api-assets
+// https://myProjectId.api.sanity.io/v2021-03-25/assets/images/myDataset
+
+export const assetsURL = `https://${process.env.SANITY_PROJECT_ID}.api.sanity.io/v2021-03-25/assets/images/${process.env.SANITY_DATASET}`;
